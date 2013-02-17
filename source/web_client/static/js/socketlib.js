c814ba5fd169cbc8827877a393ecd5f3c1083963
Harrington Joseph <h.josephv@gmail.com>
2013-02-17 01:39:51 -0800

    Fixed document.ready error

--- source/web_client/static/js/socketlib.js
+++ source/web_client/static/js/socketlib.js
@@ -1,25 +1,25 @@
-$(document).ready {
+$(document).ready( function() {
     var socket = io.connect('', {port: 8001});
     
     function updateArduinoStatus(data) {
         $('#arduinoStatusEvent').html(data['status']);    
-    }
+    };
     
     function updateClients(data) {
         $('#onlineClients').html(data.clients);
-    }
+    };
     
     function updateButtonEvents(data) {
         $('#buttonEventCount').html(data.buttonEventCount);
-    }
+    };
     
     function beep() {
         socket.emi('beep', {});
-    }
+    };
 
     $('#beepButton').click(beep);
 
     socket.on('updateArduinoStatus', updateArduinoStatus);
     socket.on('updateClients', updateClients);
     socket.on('updateButtonEvents', updateButtonEvents);
-}
+});
